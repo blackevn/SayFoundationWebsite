81db7dc2bd647c91d9d201bc9afd46a515780bcd
blackevn <kevinlouie55@gmail.com>
2023-10-17 19:31:25 +0000

    Project Pages

--- app/info/project.tsx
+++ app/info/project.tsx
@@ -5,21 +5,23 @@ export const projects: ProjectsType[] = [
     id: 123,
     name: 'School Provision',
     image: '/School.jpeg',
-    description: '',
+    description: ' Do aliqua aliquip dolor deserunt proident reprehenderit sunt reprehenderit laborum exercitation adipisicing reprehenderit elit cillum. Reprehenderit aliquip minim duis dolore et aliquip id sint exercitation do proident anim. Cupidatat ipsum et tempor dolore in veniam officia occaecat reprehenderit. Consectetur enim ea excepteur minim minim exercitation. Amet consectetur amet elit magna pariatur et. Do aliqua aliquip dolor deserunt proident reprehenderit sunt reprehenderit laborum exercitation adipisicing reprehenderit elit cillum. Reprehenderit aliquip minim duis dolore et aliquip id sint exercitation do proident anim. Cupidatat ipsum et tempor dolore in veniam officia occaecat reprehenderit. Consectetur enim ea excepteur minim minim exercitation. Amet consectetur amet elit magna pariatur et. Do aliqua aliquip dolor deserunt proident reprehenderit sunt reprehenderit laborum exercitation adipisicing reprehenderit elit cillum. Reprehenderit aliquip minim duis dolore et aliquip id sint exercitation do proident anim. Cupidatat ipsum et tempor dolore in veniam officia occaecat reprehenderit. Consectetur enim ea excepteur minim minim exercitation. Amet consectetur amet elit magna pariatur et.' 
+    ,
     link: `/projects/${123}`
+
     },
     {
     id: 234,
     name: 'Single Business Help',
     image: '/Fan.jpeg',
-    description: '',
+    description:  'Do aliqua aliquip dolor deserunt proident reprehenderit sunt reprehenderit laborum exercitation adipisicing reprehenderit elit cillum. Reprehenderit aliquip minim duis dolore et aliquip id sint exercitation do proident anim. Cupidatat ipsum et tempor dolore in veniam officia occaecat reprehenderit. Consectetur enim ea excepteur minim minim exercitation. Amet consectetur amet elit magna pariatur et.',
     link: `/projects/${234}`
     },
     {
     id: 345,
     name: 'Building Project',
     image: '/Building.png',
-    description: '',
+    description:  'Do aliqua aliquip dolor deserunt proident reprehenderit sunt reprehenderit laborum exercitation adipisicing reprehenderit elit cillum. Reprehenderit aliquip minim duis dolore et aliquip id sint exercitation do proident anim. Cupidatat ipsum et tempor dolore in veniam officia occaecat reprehenderit. Consectetur enim ea excepteur minim minim exercitation. Amet consectetur amet elit magna pariatur et.',
     link: `/projects/${345}`
     },
 ]
--- app/projects/[projectID]/page.tsx
+++ app/projects/[projectID]/page.tsx
@@ -8,7 +8,6 @@ import { usePathname, useRouter } from 'next/navigation';
 import { FaArrowCircleLeft } from 'react-icons/fa';
 
 const Project = () => {
-
 const pathname: any = usePathname();
 const projectID: number = pathname.match(/\d+/)[0].toString(); 
 const filteredProject = projects.filter(project => project.id?.toString() === projectID.toString())
@@ -17,16 +16,19 @@ const router = useRouter()
 console.log(filteredProject);
 
 
-    return <div className='pt-20 px-8'>
+    return <div className='pt-20 px-8 min-h-screen w-screen'>
           <Button
           clickEvent={router.back}
           text="Back" 
           icon={FaArrowCircleLeft}
           modifier='btn cursor-pointer'
           />
-            <div className='text-3xl font-bold pt-10'>{filteredProject.map(project => project.name)}</div>
-            <div className='w-screen grid place-items-center pt-20'>
+            <div className='text-3xl font-bold py-10'>{filteredProject.map(project => project.name)}</div>
+            <div className='w-screen grid place-items-center'>
             <img alt='image' className=' rounded-lg max-w-4xl' src={filteredProject.map(project => project.image).toString()}/>
+            </div>
+            <div className='grid place-items-center py-8 w-screen'>
+              <p className='max-w-[700px]'>{filteredProject.map(project => project.description)}</p>
             </div>
           </div>
   

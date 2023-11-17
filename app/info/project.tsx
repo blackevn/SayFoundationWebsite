4e3af969df4e243c744a182706c3a4ab526265b4
blackevn <kevinlouie55@gmail.com>
2023-11-17 19:10:21 +0000

    Menu Toggle

--- app/info/project.tsx
+++ app/info/project.tsx
@@ -5,22 +5,22 @@ export const projects: ProjectsType[] = [
     id: 123,
     name: 'School Provision',
     image: '/School.jpeg',
-    description: "Providing schools through charitable initiatives is a transformative way to address educational disparities and empower communities. Here's an overview of how a charity organization could approach school provision as a cause . We identify areas or communities that lack adequate educational infrastructure. This involves assessing factors such as limited access to schools, overcrowded classrooms, lack of resources, or remote locations",
+    description: "Providing for schools through charitable initiatives is a transformative way to address educational disparities and empower communities. Here's an overview of how a charity organization could approach school provision as a cause . We identify areas or communities that lack adequate educational infrastructure. This involves assessing factors such as limited access to schools, overcrowded classrooms, lack of resources, or remote locations",
     link: `/projects/${123}`
 
     },
-    {
-    id: 234,
-    name: 'Single Business Help',
-    image: '/Fan.jpeg',
-    description:  "Join us in illuminating hearts and unifying a community in Ghana's Northern Region. Through our mosque-building initiative, we're fostering hope, faith, and togetherness for a brighter, more inclusive future. Together, let's create a sacred space that echoes with prayers and resonates with the spirit of unity.",
-    link: `/projects/${234}`
-    },
+    // {
+    // id: 234,
+    // name: 'Single Business Help',
+    // image: '/Fan.jpeg',
+    // description:  "Join us in illuminating hearts and unifying a community in Ghana's Northern Region. Through our mosque-building initiative, we're fostering hope, faith, and togetherness for a brighter, more inclusive future. Together, let's create a sacred space that echoes with prayers and resonates with the spirit of unity.",
+    // link: `/projects/${234}`
+    // },
     {
     id: 345,
     name: 'Building Project',
     image: '/Building.png',
-    description:  'Do aliqua aliquip dolor deserunt proident reprehenderit sunt reprehenderit laborum exercitation adipisicing reprehenderit elit cillum. Reprehenderit aliquip minim duis dolore et aliquip id sint exercitation do proident anim. Cupidatat ipsum et tempor dolore in veniam officia occaecat reprehenderit. Consectetur enim ea excepteur minim minim exercitation. Amet consectetur amet elit magna pariatur et.',
+    description:  "Join us in illuminating hearts and unifying a community in Ghana's Northern Region. Through our mosque-building initiative, we're fostering hope, faith, and togetherness for a brighter, more inclusive future. Together, let's create a sacred space that echoes with prayers and resonates with the spirit of unity.",
     link: `/projects/${345}`
     },
 ]
